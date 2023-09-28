e5e960a259deb4157c40fe4b0f5bba91a6aa8f1b
p_vit <ferondos@gmai.com>
2023-09-28 16:00:37 +0300

    добавил OrderResponseDto

--- src/main/java/org/order/config/AppConfig.java
+++ src/main/java/org/order/config/AppConfig.java
@@ -1,16 +1,10 @@
 package org.order.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class AppConfig {
 
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return new AuditorAwareImpl();
-    }
 }
---
+++ src/main/java/org/order/dto/response/OrderResponseDto.java
@@ -0,0 +1,7 @@
+package org.order.dto.response;
+
+import java.util.UUID;
+
+public record OrderResponseDto(UUID orderId) {
+
+}
